ed88d95799e782266522f38498c582e77eb4c916
WenxuanZheng0 <97931921+WenxuanZheng0@users.noreply.github.com>
2022-01-20 14:44:08 +0800

    texture(remain)

---
+++ DetensionProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///
+///</summary>
+public class CameraController : MonoBehaviour
+{
+    private float MouseX;
+    private float MouseY;
+    public float XMouseSensitivity;
+    public float YMouseSensitivity;
+    public Transform playerTransform;
+
+    private void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X") * Time.deltaTime * XMouseSensitivity;
+        MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * YMouseSensitivity;
+
+
+        playerTransform.Rotate(Vector3.up * MouseX);
+        if(-160 < transform.rotation.x && transform.rotation.x < 160)
+        {
+            transform.Rotate(-Vector3.right * MouseY);
+        }
+    }
+}
---
+++ DetensionProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///
+///</summary>
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Vector3 movement;
+
+    private void Update()
+    {
+        movement.z = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Jump");
+        transform.position += transform.forward * movement.z * speed * Time.deltaTime;
+        transform.position += transform.right * movement.x * speed * Time.deltaTime;
+    }
+}
